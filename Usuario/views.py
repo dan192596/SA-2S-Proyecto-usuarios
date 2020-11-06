30b3f9748df7e2f5f796da490d2a6fad796f4f0d
Joel Daniel Perez Camey <dperez@yetis.io>
2020-11-05 19:20:48 -0600

    lecturar jwt

--- Usuario/views.py
+++ Usuario/views.py
@@ -10,9 +10,12 @@ from django.contrib.auth.hashers import make_password, check_password
 
 from .serializers import UserSerializer
 
+from jwt.contrib.algorithms.pycrypto import RSAAlgorithm
+
 import requests
 import random
 import os
+import jwt
 
 
 class LoginView(APIView):
@@ -44,6 +47,25 @@ class UserView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, id_user):
+        if os.environ['REVISAR_JWT'] =='True':
+            try:
+                authorizationHeader = request.META.get('HTTP_AUTHORIZATION')
+                token = authorizationHeader.split()            
+                f = open(os.environ['PUBLIC_JWT'], "r")
+                public_key = f.read()
+                jwt.unregister_algorithm('RS256')
+                jwt.register_algorithm('RS256', RSAAlgorithm(RSAAlgorithm.SHA256))
+                data = jwt.decode(token[1], public_key, audience='2' ,algorithm='RS256')
+                valid = False            
+                for scope in data['scopes']:
+                    if scope == "usuarios.jugadores.get":
+                        valid = True
+                if not valid:
+                    print("Token invalido")
+                    return Response(status=status.HTTP_401_UNAUTHORIZED)
+                print("Token Valido")
+            except:
+                return Response(status=status.HTTP_401_UNAUTHORIZED)
         if not User.objects.filter(username = id_user).exists():
             return Response({"detail":"Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
         user = User.objects.get(username = id_user)
@@ -56,6 +78,25 @@ class UserView(APIView):
         return Response(response, status=status.HTTP_200_OK)
 
     def put(self, request, id_user):
+        if os.environ['REVISAR_JWT'] =='True':
+            try:
+                authorizationHeader = request.META.get('HTTP_AUTHORIZATION')
+                token = authorizationHeader.split()            
+                f = open(os.environ['PUBLIC_JWT'], "r")
+                public_key = f.read()
+                jwt.unregister_algorithm('RS256')
+                jwt.register_algorithm('RS256', RSAAlgorithm(RSAAlgorithm.SHA256))
+                data = jwt.decode(token[1], public_key, audience='2' ,algorithm='RS256')
+                valid = False            
+                for scope in data['scopes']:
+                    if scope == "torneos.partida.put":
+                        valid = True
+                if not valid:
+                    print("Token invalido")
+                    return Response(status=status.HTTP_401_UNAUTHORIZED)
+                print("Token Valido")
+            except:
+                return Response(status=status.HTTP_401_UNAUTHORIZED)
         if not User.objects.filter(username = id_user).exists():
             return Response({"detail":"Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
         if id_user != int(request.data['id']) and User.objects.filter(username = request.data['id']).exists():
@@ -80,6 +121,25 @@ class CreateUserView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
+        if os.environ['REVISAR_JWT'] =='True':
+            try:
+                authorizationHeader = request.META.get('HTTP_AUTHORIZATION')
+                token = authorizationHeader.split()            
+                f = open(os.environ['PUBLIC_JWT'], "r")
+                public_key = f.read()
+                jwt.unregister_algorithm('RS256')
+                jwt.register_algorithm('RS256', RSAAlgorithm(RSAAlgorithm.SHA256))
+                data = jwt.decode(token[1], public_key, audience='2' ,algorithm='RS256')
+                valid = False            
+                for scope in data['scopes']:
+                    if scope == "torneos.partida.post":
+                        valid = True
+                if not valid:
+                    print("Token invalido")
+                    return Response(status=status.HTTP_401_UNAUTHORIZED)
+                print("Token Valido")
+            except:
+                return Response(status=status.HTTP_401_UNAUTHORIZED)
         if not 'id' in request.data or not 'nombres' in request.data or not 'apellidos' in request.data or not 'administrador' in request.data:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         if User.objects.filter(username = request.data['id']).exists():
@@ -100,7 +160,26 @@ class CreateUserView(APIView):
         }
         return Response(response, status=status.HTTP_201_CREATED)
 
-    def get(self, request):        
+    def get(self, request):
+        if os.environ['REVISAR_JWT'] =='True':
+            try:
+                authorizationHeader = request.META.get('HTTP_AUTHORIZATION')
+                token = authorizationHeader.split()            
+                f = open(os.environ['PUBLIC_JWT'], "r")
+                public_key = f.read()
+                jwt.unregister_algorithm('RS256')
+                jwt.register_algorithm('RS256', RSAAlgorithm(RSAAlgorithm.SHA256))
+                data = jwt.decode(token[1], public_key, audience='2' ,algorithm='RS256')
+                valid = False            
+                for scope in data['scopes']:
+                    if scope == "torneos.partida.get":
+                        valid = True
+                if not valid:
+                    print("Token invalido")
+                    return Response(status=status.HTTP_401_UNAUTHORIZED)
+                print("Token Valido")
+            except:
+                return Response(status=status.HTTP_401_UNAUTHORIZED)
         user = User.objects.all()
         serializer = UserSerializer(user, many=True, context={'request': request})
         response = {
